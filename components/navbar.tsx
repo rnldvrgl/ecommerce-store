a9f6864b195a6641097c5559fc396b140cf49396
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-07-27 16:37:13 +0800

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,16 @@
+import Container from "@/components/ui/container";
+import Link from "next/link";
+
 const Navbar = () => {
     return (
         <div className="border-b">
-            Navbar
+            <Container>
+                <div className="relative flex items-center h-16 px-4 sm:px-6 lg:px-8">
+                    <Link href="/" className="flex ml-4 lg:ml-0 gap-x-2">
+                        <p className="text-xl font-bold">STORE</p>
+                    </Link>
+                </div>
+            </Container>
         </div>
     );
 }
