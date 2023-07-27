e8145d2a590f705d134db80b29b856c08d653c61
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-07-27 16:30:34 +0800

    add navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Urbanist } from 'next/font/google'
 import Footer from '@/components/footer'
+import Navbar from '@/components/navbar'
 
 const urbanist = Urbanist({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={urbanist.className}>
+        <Navbar />
         {children}
         <Footer />
       </body>
