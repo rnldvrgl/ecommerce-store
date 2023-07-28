1b19299e272bf72297783b5208da5aab9867334a
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-07-28 10:52:45 +0800

    Update product-list.tsx

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,4 +1,5 @@
 import { Product } from "@/types";
+import NoResults from "./ui/no-results";
 
 interface ProductListProps {
     title: string;
@@ -12,6 +13,12 @@ const ProductList: React.FC<ProductListProps> = ({
     return (
         <div className="space-y-4">
             <h3 className="text-3xl font-bold">{title}</h3>
+            {items.length === 0 && (
+                <NoResults />
+            )}
+            <div>
+
+            </div>
         </div>
     );
 }
