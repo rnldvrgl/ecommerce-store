dc2922bd30b237d2126db124976f408cdca7b5c5
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-07-28 16:41:49 +0800

    Update icon-button.tsx

--- components/ui/icon-button.tsx
+++ components/ui/icon-button.tsx
@@ -5,7 +5,7 @@ import { MouseEventHandler } from "react";
 interface IconButtonProps {
     onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
     icon: React.ReactElement;
-    className: string;
+    className?: string;
 }
 
 const IconButton: React.FC<IconButtonProps> = ({
