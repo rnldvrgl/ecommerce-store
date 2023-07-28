91e619b3bed5adc487653055d41a1fcff2f2d217
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-07-28 17:43:54 +0800

    Update navbar-actions.tsx

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -1,17 +1,20 @@
-"use client"
-import { useState, useEffect } from "react"
-import Button from "@/components/ui/button"
+"use client";
+
 import { ShoppingBag } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+import Button from "@/components/ui/button";
 import useCart from "@/hooks/use-cart";
 
 const NavbarActions = () => {
-
     const [isMounted, setIsMounted] = useState(false);
 
     useEffect(() => {
-        setIsMounted(true)
-    }, [])
+        setIsMounted(true);
+    }, []);
 
+    const router = useRouter();
     const cart = useCart();
 
     if (!isMounted) {
@@ -20,8 +23,11 @@ const NavbarActions = () => {
 
     return (
         <div className="flex items-center ml-auto gap-x-4">
-            <Button className="flex justify-center px-4 py-2 bg-black rounded-full item-center ">
-                <ShoppingBag size={20} color="white" />
+            <Button onClick={() => router.push('/cart')} className="flex items-center px-4 py-2 bg-black rounded-full">
+                <ShoppingBag
+                    size={20}
+                    color="white"
+                />
                 <span className="ml-2 text-sm font-medium text-white">
                     {cart.items.length}
                 </span>
