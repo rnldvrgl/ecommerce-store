13781376e7976c39d3956dc32bbb4b60b0fb8beb
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-07-28 11:08:10 +0800

    update product card

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,5 +1,6 @@
 import { Product } from "@/types";
 import NoResults from "./ui/no-results";
+import ProductCard from "./ui/product-card";
 
 interface ProductListProps {
     title: string;
@@ -18,9 +19,7 @@ const ProductList: React.FC<ProductListProps> = ({
             )}
             <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
                 {items.map((item) => (
-                    <div key={item.id}>
-                        {item.name}
-                    </div>
+                    <ProductCard key={item.id} data={item} />
                 ))}
             </div>
         </div>
---
+++ components/ui/product-card.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import { Product } from "@/types";
+import Image from "next/image";
+
+interface ProductCard {
+    data: Product;
+}
+
+const ProductCard: React.FC<ProductCard> = ({
+    data
+}) => {
+    return (
+        <div className="p-3 space-y-4 bg-white border cursor-pointer group rounded-xl">
+            {/* Images and Actions */}
+            <div className="relative bg-gray-100 aspect-square rounded-xl">
+                <Image
+                    src={data?.images?.[0]?.url}
+                    fill
+                    alt="image"
+                    className="object-cover rounded-md aspect-square"
+                />
+            </div>
+        </div>
+    );
+}
+
+export default ProductCard;
