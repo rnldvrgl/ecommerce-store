ae61de6fff0b2c65a95cd9fe14bc8561d8fd3974
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-07-28 10:16:53 +0800

    update get-products

--- actions/get-products.tsx
+++ actions/get-products.tsx
@@ -1,8 +1,8 @@
 import { Product } from "@/types";
 
-const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories`;;
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`;;
 
-const getProducts = async (): Promise<Category[]> => {
+const getProducts = async (): Promise<Product[]> => {
     const res = await fetch(URL);
 
     return res.json();
