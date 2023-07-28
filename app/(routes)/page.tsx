e04a9f1ba093fd3b6e7493b15d2dfa3a39fe9f34
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-07-28 10:38:05 +0800

    Update page.tsx

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,16 +1,21 @@
 import Container from "@/components/ui/container"
 import Billboard from "@/components/billboard";
 import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
 
 export const revalidate = 0;
 
 const HomePage = async () => {
+    const products = await getProducts({ isFeatured: true });
     const billboard = await getBillboard('0821ef03-1c8d-4886-8847-79ffbe99f272');
 
     return (
         <Container>
             <div className="pb-10 space-y-10">
                 <Billboard data={billboard} />
+            </div>
+            <div className="flex flex-col px-4 gap-y-8 sm:px-6 lg:px-8">
+                <ProductList title="Feature Products" items={products} />
             </div>
         </Container>
     )
