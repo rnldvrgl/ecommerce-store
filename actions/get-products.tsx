2c481144d3a722516123a987d22ebd03fd7651b3
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-07-28 10:31:27 +0800

    Update get-products.tsx

--- actions/get-products.tsx
+++ actions/get-products.tsx
@@ -1,8 +1,26 @@
 import { Product } from "@/types";
+import qs from "query-string"
 
-const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`;;
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`;
 
-const getProducts = async (): Promise<Product[]> => {
+interface Query {
+    categoryId?: string;
+    colorId?: string;
+    sizeId?: string;
+    isFeatured: boolean;
+}
+
+const getProducts = async (query: Query): Promise<Product[]> => {
+    const url = qs.stringifyUrl({
+        url: URL,
+        query: {
+            colorId: query.colorId,
+            sizeId: query.sizeId,
+            categoryId: query.categoryId,
+            isFeatured: query.isFeatured
+        }
+    })
+
     const res = await fetch(URL);
 
     return res.json();
