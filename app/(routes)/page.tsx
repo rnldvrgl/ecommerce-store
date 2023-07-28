f524875d4af993141031ad9fd3b2558811d45818
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-07-28 09:41:15 +0800

    fetch billboard

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,10 +1,16 @@
 import Container from "@/components/ui/container"
 import Billboard from "@/components/billboard";
-const HomePage = () => {
+import getBillboard from "@/actions/get-billboard";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+    const billboard = await getBillboard('0821ef03-1c8d-4886-8847-79ffbe99f272');
+
     return (
         <Container>
             <div className="pb-10 space-y-10">
-                {/* <Billboard data={ } /> */}
+                <Billboard data={billboard} />
             </div>
         </Container>
     )
